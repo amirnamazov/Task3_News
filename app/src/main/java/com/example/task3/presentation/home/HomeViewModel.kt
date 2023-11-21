b0a483b184de1e85503b09865675e8756a8ded2d
amir <amir1990@gmail.com>
2023-11-21 16:12:58 +0400

    Revert "viewmodels modified"
    This reverts commit adad0589351c3b638ac84f2d75e7d96bcb5d6b72.

--- app/src/main/java/com/example/task3/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/task3/presentation/home/HomeViewModel.kt
@@ -3,24 +3,26 @@ package com.example.task3.presentation.home
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.task3.common.ResourceState
 import com.example.task3.data.data_source.remote.dto.news.NewsDTO
 import com.example.task3.domain.model.Article
 import com.example.task3.domain.use_cases.NewsApiUseCase
-import com.example.task3.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @OptIn(FlowPreview::class)
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val useCase: NewsApiUseCase) : BaseViewModel() {
+class HomeViewModel @Inject constructor(private val useCase: NewsApiUseCase) : ViewModel() {
 
     @Inject
     lateinit var sharedPref: SharedPreferences
@@ -38,13 +40,13 @@ class HomeViewModel @Inject constructor(private val useCase: NewsApiUseCase) : B
     val resHeadlines: LiveData<HomeUIState> get() = _resHeadlines
 
     fun fetchHeadlines() =
-        useCase.getHeadlines(mapHeadLine()).fetchData(_resHeadlines) { setUIState(it) }
+        useCase.getHeadlines(mapHeadLine()).fetchData(_resHeadlines)
 
     private val _resNews = MutableLiveData<HomeUIState>()
     val resNews: LiveData<HomeUIState> get() = _resNews
 
-    fun fetchNews(query: String = "tesla") = useCase
-        .getEverything(mapEveryThing(query)).fetchData(_resNews) { setUIState(it) }
+    fun fetchNews(query: String = "tesla") =
+        useCase.getEverything(mapEveryThing(query)).fetchData(_resNews)
 
     private val sharedFlowSearch = MutableSharedFlow<String>(
         extraBufferCapacity = 1,
@@ -59,14 +61,21 @@ class HomeViewModel @Inject constructor(private val useCase: NewsApiUseCase) : B
         }
     }
 
-    private fun setUIState(res: ResourceState<NewsDTO>): HomeUIState = when (res) {
-        is ResourceState.ConnectionError -> HomeUIState.ConnectionError(res.message!!)
-        is ResourceState.Error -> HomeUIState.Error(res.message!!)
-        is ResourceState.Loading -> HomeUIState.Loading
-        is ResourceState.Success ->
-            if (res.data?.articles.isNullOrEmpty()) HomeUIState.Empty
-            else HomeUIState.Success(res.data!!.articles!!.format())
-    }
+    private fun Flow<ResourceState<NewsDTO>>.fetchData(livedata: MutableLiveData<HomeUIState>) =
+        viewModelScope.launch(Dispatchers.IO) {
+            this@fetchData.collect { res ->
+                withContext(Dispatchers.Main) {
+                    livedata.value = when (res) {
+                        is ResourceState.ConnectionError -> HomeUIState.ConnectionError(res.message!!)
+                        is ResourceState.Error -> HomeUIState.Error(res.message!!)
+                        is ResourceState.Loading -> HomeUIState.Loading
+                        is ResourceState.Success ->
+                            if (res.data?.articles.isNullOrEmpty()) HomeUIState.Empty
+                            else HomeUIState.Success(res.data!!.articles!!.format())
+                    }
+                }
+            }
+        }
 
     private fun List<Article>.format(): List<Article> = map {
         val dateFormatted = it.publishedAt?.substring(0, 10)
--- app/src/main/java/com/example/task3/presentation/saved/SavedViewModel.kt
+++ app/src/main/java/com/example/task3/presentation/saved/SavedViewModel.kt
@@ -2,27 +2,34 @@ package com.example.task3.presentation.saved
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.task3.common.ResourceState
-import com.example.task3.data.data_source.local.model.ArticleModel
 import com.example.task3.domain.use_cases.NewsLocalUseCase
-import com.example.task3.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class SavedViewModel @Inject constructor(private val useCase: NewsLocalUseCase) : BaseViewModel() {
+class SavedViewModel @Inject constructor(private val useCase: NewsLocalUseCase) : ViewModel() {
 
     private val _articleList = MutableLiveData<SavedUIState>()
     val articleList: LiveData<SavedUIState> get() = _articleList
 
-    fun getArticleList() = useCase.getAll().fetchData(_articleList) { setUIState(it) }
-
-    private fun setUIState(res: ResourceState<List<ArticleModel>>): SavedUIState = when (res) {
-        is ResourceState.ConnectionError -> SavedUIState.Error(res.message!!)
-        is ResourceState.Error -> SavedUIState.Error(res.message!!)
-        is ResourceState.Loading -> SavedUIState.Loading
-        is ResourceState.Success ->
-            if (res.data.isNullOrEmpty()) SavedUIState.Empty
-            else SavedUIState.Success(res.data)
+    fun getArticleList() = viewModelScope.launch(Dispatchers.IO) {
+        useCase.getAll().collect { res ->
+            withContext(Dispatchers.Main) {
+                _articleList.value = when (res) {
+                    is ResourceState.ConnectionError -> SavedUIState.Error(res.message!!)
+                    is ResourceState.Error -> SavedUIState.Error(res.message!!)
+                    is ResourceState.Loading -> SavedUIState.Loading
+                    is ResourceState.Success ->
+                        if (res.data.isNullOrEmpty()) SavedUIState.Empty
+                        else SavedUIState.Success(res.data)
+                }
+            }
+        }
     }
 }
