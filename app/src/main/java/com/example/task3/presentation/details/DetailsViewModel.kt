adad0589351c3b638ac84f2d75e7d96bcb5d6b72
amir <amir1990@gmail.com>
2023-11-21 16:09:21 +0400

    viewmodels modified

--- app/src/main/java/com/example/task3/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/task3/presentation/details/DetailsViewModel.kt
@@ -2,21 +2,19 @@ package com.example.task3.presentation.details
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.task3.common.ResourceState
 import com.example.task3.data.data_source.local.model.ArticleModel
 import com.example.task3.domain.use_cases.NewsLocalUseCase
+import com.example.task3.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsViewModel @Inject constructor(private val useCase: NewsLocalUseCase) : ViewModel() {
+class DetailsViewModel @Inject constructor(private val useCase: NewsLocalUseCase) : BaseViewModel() {
 
     suspend fun checkExistenceInDb(model: ArticleModel, saved: (Boolean) -> Unit) = useCase.getAll()
         .filter { it is ResourceState.Success }
@@ -30,7 +28,7 @@ class DetailsViewModel @Inject constructor(private val useCase: NewsLocalUseCase
 
     fun saveArticle(model: ArticleModel) = viewModelScope.launch(Dispatchers.IO) {
         checkExistenceInDb(model) { saved ->
-            if (!saved) useCase.insert(model).getResponse(_articleSave)
+            if (!saved) useCase.insert(model).fetchData(_articleSave) { setUIState(it)!! }
         }
     }
 
@@ -38,20 +36,13 @@ class DetailsViewModel @Inject constructor(private val useCase: NewsLocalUseCase
     val articleRemove: LiveData<DetailsState> get() = _articleRemove
 
     fun removeArticle(id: Int) =
-        if (id != 0) useCase.delete(id).getResponse(_articleRemove)
-        else useCase.deleteLast().getResponse(_articleRemove)
+        if (id != 0) useCase.delete(id).fetchData(_articleRemove) { setUIState(it)!! }
+        else useCase.deleteLast().fetchData(_articleRemove) { setUIState(it)!! }
 
-    private fun Flow<ResourceState<Unit>>.getResponse(liveData: MutableLiveData<DetailsState>) =
-        viewModelScope.launch(Dispatchers.IO) {
-            this@getResponse.collect { res ->
-                withContext(Dispatchers.Main) {
-                    liveData.value = when (res) {
-                        is ResourceState.ConnectionError -> DetailsState.Error(res.message!!)
-                        is ResourceState.Error -> DetailsState.Error(res.message!!)
-                        is ResourceState.Loading -> null
-                        is ResourceState.Success -> DetailsState.Success()
-                    }
-                }
-            }
-        }
+    private fun setUIState(res: ResourceState<Unit>): DetailsState? = when (res) {
+        is ResourceState.ConnectionError -> DetailsState.Error(res.message!!)
+        is ResourceState.Error -> DetailsState.Error(res.message!!)
+        is ResourceState.Loading -> null
+        is ResourceState.Success -> DetailsState.Success()
+    }
 }
